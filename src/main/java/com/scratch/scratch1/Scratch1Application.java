613506e03fe833ea13315268c3b471b5d793114f
Dan <dankim0213@gmail.com>
2020-12-28 18:03:03 +0900

    initialized scratch repository for Spring Framework

---
+++ src/main/java/com/scratch/scratch1/Scratch1Application.java
@@ -0,0 +1,13 @@
+package com.scratch.scratch1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scratch1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Scratch1Application.class, args);
+	}
+
+}
